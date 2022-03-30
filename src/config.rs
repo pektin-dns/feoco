786f8c5499174861e989964bda91fdf442475f9b
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-03-30 18:56:00 +0200

    add config; add performance checks

--- src/config.rs
+++ src/config.rs
@@ -1,56 +1,56 @@
 use regex::Regex;
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Config {
     pub headers: Headers,
 }
-
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Headers {
-    #[serde(rename = "Content-Security-Policy")]
-    pub content_security_policy: String,
-    #[serde(rename = "x-frame-options")]
-    pub x_frame_options: String,
-    #[serde(rename = "x-content-type-options")]
-    pub x_content_type_options: String,
-    #[serde(rename = "x-permitted-cross-domain-policies")]
-    pub x_permitted_cross_domain_policies: String,
-    #[serde(rename = "x-download-options")]
-    pub x_download_options: String,
-    #[serde(rename = "x-xss-protection")]
-    pub x_xss_protection: String,
-    #[serde(rename = "referrer-policy")]
-    pub referrer_policy: String,
-    #[serde(rename = "Strict-Transport-Security")]
-    pub strict_transport_security: String,
-    #[serde(rename = "feature-policy")]
-    pub feature_policy: String,
-    #[serde(rename = "Cache-Control")]
-    pub cache_control: String,
+    pub all: HashMap<String, String>,
+    pub document: HashMap<String, String>,
 }
+
 const CONFIG_PATH: &str = "/config.yml";
 
 pub fn read_config() -> Config {
     let config_file = read_to_string(CONFIG_PATH).unwrap();
     let config_file = replace_variables(config_file);
-    let config: Config = serde_yaml::from_str(&config_file).unwrap();
+    let mut config: Config = serde_yaml::from_str(&config_file).unwrap();
+    let replace_spaces = Regex::new(r"\s+").unwrap();
+    for header in config.headers.all.clone() {
+        config.headers.all.insert(
+            header.0.to_string(),
+            replace_spaces
+                .replace_all(header.1.replace('\n', " ").as_str(), " ")
+                .to_string(),
+        );
+    }
+    for header in config.headers.document.clone() {
+        config.headers.document.insert(
+            header.0.to_string(),
+            replace_spaces
+                .replace_all(header.1.replace('\n', " ").as_str(), " ")
+                .to_string(),
+        );
+    }
     config
 }
 
 pub fn replace_variables(config_file: String) -> String {
-    let mut config_file = config_file;
+    let config_file = config_file;
+    let mut config_file_repl = String::from("");
     for matsch in Regex::new(r"\$\{[A-Z_]+\}")
         .unwrap()
-        .find_iter(&config_file.clone())
+        .find_iter(&config_file)
     {
         let variable = matsch.as_str();
         let variable = variable.replace("${", "").replace('}', "");
         let variable_value = std::env::var(variable).unwrap_or_else(|_| "".to_string());
-        config_file = config_file.replace(matsch.as_str(), &variable_value);
+        config_file_repl = config_file.replace(matsch.as_str(), &variable_value);
     }
 
-    config_file
+    config_file_repl
 }
